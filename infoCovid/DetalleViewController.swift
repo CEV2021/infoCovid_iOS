3c9a6072f83893e6ecd73681a3705e1bb988902f
Daniel <floresclifman@gmail.com>
2021-03-23 23:11:47 +0100

    Notificaciones (Settings) persistencia añadida

--- infoCovid/DetalleViewController.swift
+++ infoCovid/DetalleViewController.swift
@@ -7,11 +7,22 @@ class DetalleViewController: UIViewController {
     @IBOutlet weak var totalInfectionsLabel: UILabel!
     @IBOutlet weak var sevenDaysView: UIStackView!
     @IBOutlet weak var conditionImage: UIImageView!
+    let kMKey = "MY_KEY"
+    var notifications: Bool?
     
-    var infectionsNumber : Int = 800
+    // Constante con la que manejamos los elementos de settings
+    let settings = SettingsViewController()
+    
+    var infectionsNumber : Int = 200
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        settings.setDefaultValues()
+        notifications = UserDefaults.standard.bool(forKey: kMKey)
+        print(notifications!)
+        
+        
         
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
             (granted, error) in
@@ -22,7 +33,7 @@ class DetalleViewController: UIViewController {
                 print(error.debugDescription)
             }
         }
-    
+        
         sevenDaysView.layer.cornerRadius = 20
         sevenDaysView.layer.borderWidth = 3
         conditionImageControl()
@@ -35,31 +46,39 @@ class DetalleViewController: UIViewController {
             conditionImage.image = UIImage.init(named: "coronavirusRojo")
             self.showNotification(text: (self.totalInfectionsLabel.text ?? ""), subtitle: "ALTO")
             
+            
+            
         }else if infectionsNumber > 400{
             conditionImage.image = UIImage.init(named: "coronavirusAma")
             self.showNotification(text: (self.totalInfectionsLabel.text ?? ""), subtitle: "MEDIO")
+            
+            
         }else{
             conditionImage.image = UIImage.init(named: "coronavirusVerde")
             self.showNotification(text: (self.totalInfectionsLabel.text ?? ""), subtitle: "BAJO")
+            
+            
         }
     }
     
     // se crea la funcion para las notificaciones
     func showNotification(text: String, subtitle: String){
-        let content = UNMutableNotificationContent()
-        content.title = "Nivel de alerta actual"
-        content.subtitle = subtitle
-        content.body = "La incidencia es de: \(text) contagios"
-        content.sound = .default
-        content.badge = 1
-        //se crea el trigger
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
-        //Creamos la request y añadidos el content del trigger
-        let request = UNNotificationRequest(identifier: "Mi Notificacion", content: content, trigger: trigger)
-        //añadimos la notificacion al centro de notificaciones
-        UNUserNotificationCenter.current().add(request) { (error) in
-            
-            print (error.debugDescription)
+        if notifications! {
+            let content = UNMutableNotificationContent()
+            content.title = "Nivel de alerta actual"
+            content.subtitle = subtitle
+            content.body = "La incidencia es de: \(text) contagios"
+            content.sound = .default
+            content.badge = 1
+            //se crea el trigger
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 2, repeats: false)
+            //Creamos la request y añadidos el content del trigger
+            let request = UNNotificationRequest(identifier: "Mi Notificacion", content: content, trigger: trigger)
+            //añadimos la notificacion al centro de notificaciones
+            UNUserNotificationCenter.current().add(request) { (error) in
+                
+                print (error.debugDescription)
+            }
         }
     }
 }
--- infoCovid/SettingsViewController.swift
+++ infoCovid/SettingsViewController.swift
@@ -3,32 +3,48 @@ import UIKit
 
 class SettingsViewController: UIViewController{
     
+    var notifications: Bool?
+    
+    // Constante para almacenar la clave de UserDefaults
+    let kMyKey = "MY_KEY"
+    
     @IBOutlet weak var notificationsSwitch: UISwitch!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
+        
+        setDefaultValues()
+        // Obtenemos el estado de las notificaciones del almacenamiento interno
+        notifications = UserDefaults.standard.bool(forKey: kMyKey)
+        notificationsSwitch.setOn(notifications!, animated: true)
+        
+        
     }
     @IBAction func notificationsAction(_ sender: Any) {
-        
-        if notificationsSwitch.isOn{
-            UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) {
-                (granted, error) in
-                if granted {
-                    print("Permiso aceptado")
-                }else{
-                    print("Sin permiso")
-                    print(error.debugDescription)
-                }
-            }
-            
-            
-        }else{
-            UNUserNotificationCenter.cancelPreviousPerformRequests(withTarget: (Any).self)
-        }
+        notifications!.toggle()
+        UserDefaults.standard.set(notifications, forKey: kMyKey)
+        UserDefaults.standard.synchronize()
+        notifications = UserDefaults.standard.bool(forKey: kMyKey)
+        print(notifications!)
         
     }
+    @IBAction func deleteData(_ sender: UIButton) {
+        UserDefaults.standard.removeObject(forKey: kMyKey)
+        UserDefaults.standard.synchronize()
+        print("Desde almacenamiento interno dato: \(UserDefaults.standard.bool(forKey: kMyKey))")
+    }
     
-
+    /* Función con la que damos el valor true por defecto
+     al booleano del almacenamiento interno, ya que de no haber nada
+     en el almacenamiento, la variable se pone a false */
+    
+    func setDefaultValues() {
+        let userDefaults = UserDefaults.standard
+        let appDefaults: [String:Any] = [ kMyKey : true]
+        userDefaults.register(defaults: appDefaults)
+    }
 }
+
+
